3223528d0b82f54d22214fc3ca28ef6b56d2df64
ocha <kodoktemler@gmail.com>
2026-06-20 00:05:35 +0800

    perbaikan website yang tidak tampil

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio-vue/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
